405d48de5718620038330db9f871372b3bebdde8
meghanashastri1 <meghana360@gmail.com>
2019-09-04 17:24:07 +0000

    updated

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+//testing main function
+int main(){
+  //string hello displays the message
+  string hello = "Hello World";
+  //send string as an output to the console
+  cout << hello << endl;
+  //return exit_success if there are no errors 
+  return EXIT_SUCCESS;
+}
